cc7e4ef870be560f47a8134a5268069dc21f41b1
Dan Dyer <dan@uncommons.org>
2006-09-21 21:59:22 +0000

    Added licence details.

--- reportng/src/java/org/uncommons/reportng/ReportNGUtils.java
+++ reportng/src/java/org/uncommons/reportng/ReportNGUtils.java
@@ -1,8 +1,37 @@
+// ============================================================================
+//   Copyright 2006 Daniel W. Dyer
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// ============================================================================
 package org.uncommons.reportng;
 
-import java.util.*;
-import java.text.*;
-import org.testng.*;
+import java.text.DateFormat;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import org.testng.IClass;
+import org.testng.IResultMap;
+import org.testng.ITestResult;
+import org.testng.Reporter;
 
 /**
  * Utility class that provides various helper methods that can be invoked
