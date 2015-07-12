39f81fcb55dff0358365c539711f836bd05bf720
tobecrazy <dbyl@foxmail.com>
2015-07-12 17:51:12 +0800

    fix support chinese charset

--- reportng/src/java/main/org/uncommons/reportng/AbstractReporter.java
+++ reportng/src/java/main/org/uncommons/reportng/AbstractReporter.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.OutputStream;
 import java.io.Writer;
 import java.util.ResourceBundle;
 import org.apache.velocity.VelocityContext;
